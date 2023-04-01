0d8d4a44907168ca6272468d4b37b692f7aa3363
margaryanmariam <margaryan.mariam1707@gmail.com>
2023-04-01 21:45:10 +0400

    fix selenium

--- src/main/java/CatalogPage.java
+++ src/main/java/CatalogPage.java
@@ -1,5 +1,4 @@
 import globalUse.FundamentalUse;
-import jdk.jfr.Category;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
--- src/main/java/OrderConfirmationPage.java
+++ src/main/java/OrderConfirmationPage.java
@@ -1,5 +1,5 @@
 import globalUse.FundamentalUse;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
